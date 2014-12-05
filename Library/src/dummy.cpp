ec0ed160f8e70e910f67bd2f475c68647873132e
Michael König <michael.koenig@blue-yonder.com>
2014-12-05 16:41:36 +0100

    First prototype which actually connects to the database and provides a cursor object. Works in manual tests\!

--- Library/src/dummy.cpp
+++ Library/src/dummy.cpp
@@ -11,14 +11,57 @@
  */
 
 #include <boost/python.hpp>
+#include <cpp_odbc/level1/unixodbc_backend.h>
+#include <cpp_odbc/level2/level1_connector.h>
+#include <cpp_odbc/raii_environment.h>
+#include <cpp_odbc/raii_connection.h>
+#include <cpp_odbc/raii_statement.h>
+
+#include <psapp/valid_ptr.h>
+#include <sqlext.h>
 
 namespace bp = boost::python;
+using namespace cpp_odbc;
 
 namespace pydbc {
 
-	int connect()
+	struct py_cursor {
+		psapp::valid_ptr<raii_environment> environment;
+		psapp::valid_ptr<raii_connection> connection;
+		psapp::valid_ptr<raii_statement> statement;
+
+		void execute(std::string const & sql)
+		{
+			statement->execute(sql);
+		}
+	};
+
+	struct py_connection {
+		psapp::valid_ptr<raii_environment> environment;
+		psapp::valid_ptr<raii_connection> connection;
+		psapp::valid_ptr<raii_statement> statement;
+
+		void commit()
+		{
+			auto api = environment->get_api();
+			api->end_transaction(connection->get_handle(), SQL_COMMIT);
+		}
+
+		py_cursor cursor()
+		{
+			return {environment, connection, statement};
+		}
+	};
+
+	py_connection connect(std::string const & data_source_name)
 	{
-		return 42;
+		auto l1 = psapp::make_valid_ptr<level1::unixodbc_backend const>();
+		auto l2 = psapp::make_valid_ptr<level2::level1_connector const>(l1);
+		auto environment = psapp::make_valid_ptr<raii_environment>(l2);
+		auto connection = psapp::make_valid_ptr<raii_connection>(l2, environment->get_handle(), "dsn=" + data_source_name);
+		auto statement = psapp::make_valid_ptr<raii_statement>(l2, connection->get_handle());
+
+		return {environment, connection, statement};
 	}
 
 }
@@ -26,4 +69,13 @@ namespace pydbc {
 BOOST_PYTHON_MODULE(pydbc)
 {
     bp::def("connect", pydbc::connect);
+
+    bp::class_<pydbc::py_cursor>("Cursor", bp::no_init)
+    		.def("execute", &pydbc::py_cursor::execute)
+    	;
+
+    bp::class_<pydbc::py_connection>("Connection", bp::no_init)
+    		.def("commit", &pydbc::py_connection::commit)
+    		.def("cursor", &pydbc::py_connection::cursor)
+    	;
 }
--- PyTest/test_import.py
+++ PyTest/test_import.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
+from pydbc import connect
 
 class TestImportPyDBC(TestCase):
-    def test_import(self):
-        import pydbc
+        
+    def test_connect(self):
+        self.assertEquals(17, connect("PostgreSQL R&D test database"))
         
 if __name__ == '__main__':
     from unittest import main
