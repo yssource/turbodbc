565cd8bf78f9292d75ffb8410774c7e2ceaf77b8
Michael König <michael.koenig@blue-yonder.com>
2014-12-05 09:54:44 +0100

    Working cmake scripts. Added scripts for setting up work environment

---
+++ setup_env.sh
@@ -0,0 +1,3 @@
+source /data/software/Ubuntu_12.04/psapp/latest/use_psapp.sh
+source /data/software/foundation/release/cppunit_toolbox/Ubuntu_12.04/cppunit_toolbox_0_3_0/use_cppunit_toolbox.sh
+source /data/software/Ubuntu_12.04/unixODBC/unixODBC-2.2.14/use_unixodbc.sh
