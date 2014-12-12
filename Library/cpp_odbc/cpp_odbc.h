85461958073bc721bdde6fc43bd6e4176214f27f
Michael König <michael.koenig@blue-yonder.com>
2014-12-12 15:17:53 +0100

    Added convenience includes and namespace documentation

--- Library/cpp_odbc/cpp_odbc.h
+++ Library/cpp_odbc/cpp_odbc.h
@@ -15,8 +15,8 @@
  * @brief This namespace contains all classes and functions which belong to the
  *        cpp_odbc library.
  *
- *        The main namespace only contains high-level features.
+ *        The main namespace only contains high-level features intended for
+ *        library users. A good starting point is to call cpp_odbc::make_environment()
+ *        and descend from there.
  */
-namespace cpp_odbc {
-
-}
+namespace cpp_odbc {}
