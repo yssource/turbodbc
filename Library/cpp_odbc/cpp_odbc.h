42a58e4950e6b625f336957d65d38a8219c6c350
Michael König <michael.koenig@blue-yonder.com>
2014-12-12 15:29:21 +0100

    Forgot high-level includes

--- Library/cpp_odbc/cpp_odbc.h
+++ Library/cpp_odbc/cpp_odbc.h
@@ -11,6 +11,14 @@
  *
  */
 
+#include "cpp_odbc/connection.h"
+#include "cpp_odbc/credentials.h"
+#include "cpp_odbc/environment.h"
+#include "cpp_odbc/error.h"
+#include "cpp_odbc/make_environment.h"
+#include "cpp_odbc/multi_value_buffer.h"
+#include "cpp_odbc/statement.h"
+
 /**
  * @brief This namespace contains all classes and functions which belong to the
  *        cpp_odbc library.
@@ -18,5 +26,8 @@
  *        The main namespace only contains high-level features intended for
  *        library users. A good starting point is to call cpp_odbc::make_environment()
  *        and descend from there.
+ *
+ *        The classes found here do not form a complete database driver, but should enable
+ *        users to build one themselves quickly.
  */
 namespace cpp_odbc {}
