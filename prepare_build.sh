af6b113df4f93037c2078b7ef40eadd1918ea8c1
Jan Karstens <jan.karstens@blue-yonder.com>
2014-12-18 18:03:26 +0100

    introduced BY_HOME_BASE

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,3 +1,4 @@
+BY_HOME_BASE=/home/localdata/${USER}
 mkdir -p ../cpp_odbc_build
 cd ../cpp_odbc_build 
-cmake -DCMAKE_MODULE_PATH=/home/localdata/${USER}/workspace/cmake_scripts -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../cpp_odbc
+cmake -DCMAKE_MODULE_PATH=${BY_HOME_BASE}/workspace/cmake_scripts -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../cpp_odbc
