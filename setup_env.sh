d27e5bd315c240eb06e69a67549231bbbff80077
Markus Warsinsky <Markus.Warsinsky@blue-yonder.com>
2014-12-18 15:16:12 +0100

    use os detection in setup

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,15 @@
+if [ -z "$label" ]; then 
+        if grep -q Ubuntu /etc/issue; then
+                label=Ubuntu_12.04
+        elif grep -q Enterprise /etc/issue; then
+                label=SUSE_LINUX_11
+        elif grep -q -i debian /etc/issue; then
+                label=Debian_7
+        else 
+                echo "Couldn't determine distribution and 'label' is not set"
+                exit 1
+        fi
+fi
 source /data/software/${label}/psapp/latest/use_psapp.sh
 export PSAPP_HOME=$PSAPP_HOME
 source /data/software/foundation/release/cppunit_toolbox/${label}/cppunit_toolbox_0_3_0/use_cppunit_toolbox.sh
