f40c90f022fad2f61ec304e52e05c25c32c8054e
Michael König <michael.koenig@blue-yonder.com>
2014-12-18 16:59:12 +0100

    Removed psapp from setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -10,7 +10,5 @@ if [ -z "$label" ]; then
                 exit 1
         fi
 fi
-source /data/software/${label}/psapp/latest/use_psapp.sh
-export PSAPP_HOME=$PSAPP_HOME
 source /data/software/foundation/release/cppunit_toolbox/${label}/cppunit_toolbox_0_3_0/use_cppunit_toolbox.sh
 source /data/software/${label}/unixODBC/unixODBC-2.2.14/use_unixodbc.sh
