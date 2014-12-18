9076f4a37e1bd65c3a4758ec452074b1f8d269cf
Matthias Bach <matthias.bach@blue-yonder.com>
2014-12-18 17:04:25 +0100

    Fix Pythonpath

--- setup_env.sh
+++ setup_env.sh
@@ -5,4 +5,4 @@ export CPP_ODBC_INCLUDE_DIR=/home/localdata/${USER}/workspace/cpp_odbc/Library/
 source /data/software/foundation/release/cppunit_toolbox/${label}/cppunit_toolbox_0_3_0/use_cppunit_toolbox.sh
 source /data/software/${label}/unixODBC/unixODBC-2.2.14/use_unixodbc.sh
 
-export PYTHONPATH=/home/localdata/${USER}/workspace/pydbc_build/Library/
+export PYTHONPATH=/home/localdata/${USER}/workspace/pydbc_build/
