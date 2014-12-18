34e0a255341e1cc581eb9888fffe88277d849e02
Jan Karstens <jan.karstens@blue-yonder.com>
2014-12-18 18:13:49 +0100

    ln magic to avoid error output

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,4 +1,5 @@
+set -e
 mkdir -p ../pydbc_build
 cd ../pydbc_build 
 cmake -DCMAKE_MODULE_PATH="${BY_HOME_BASE}/workspace/cmake_scripts" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../pydbc
-ln -s ../pydbc/PyTest PyTest
+ln -f -s ../pydbc/PyTest PyTest
