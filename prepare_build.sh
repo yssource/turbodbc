6a4be21e74947a82f9ef43a32c3ca49808d2554f
Matthias Bach <matthias.bach@blue-yonder.com>
2014-12-18 15:25:47 +0100

    Make workspace paths a little less user specific

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,4 +1,4 @@
 mkdir -p ../pydbc_build
 cd ../pydbc_build 
-cmake -DCMAKE_MODULE_PATH="/localdata/mkoenig/workspace/cmake_scripts;/localdata/mkoenig/workspace/pydbc/" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../pydbc
+cmake -DCMAKE_MODULE_PATH="/home/localdata/${USER}/workspace/cmake_scripts;/home/localdata/${USER}/workspace/pydbc/" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../pydbc
 ln -s ../pydbc/PyTest PyTest
