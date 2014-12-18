f52dac38633e346dbb4a3a8e1d796615fd1cd411
Michael König <michael.koenig@blue-yonder.com>
2014-12-18 19:07:39 +0100

    First version of result set which retrieves correct database error

--- Library/src/cursor.cpp
+++ Library/src/cursor.cpp
@@ -11,7 +11,8 @@
  */
 
 #include <pydbc/cursor.h>
-#include <sql.h>
+#include <sqlext.h>
+#include <stdexcept>
 
 namespace pydbc {
 
@@ -22,13 +23,19 @@ void cursor::execute(std::string const & sql)
 	statement->execute(sql);
 	if (statement->number_of_columns() != 0) {
 		buffer = std::make_shared<cpp_odbc::multi_value_buffer>(sizeof(long), cached_rows);
-		statement->bind_column(1, SQL_INTEGER, *buffer);
+		statement->bind_column(1, SQL_C_SBIGINT, *buffer);
 	}
 }
 
 std::vector<int> cursor::fetch_one()
 {
-	return {statement->number_of_columns()};
+	if (buffer) {
+		statement->fetch_next();
+		auto value_ptr = reinterpret_cast<long *>((*buffer)[0].data_pointer);
+		return {*value_ptr};
+	} else {
+		throw std::runtime_error("No active result set");
+	}
 }
 
 }
