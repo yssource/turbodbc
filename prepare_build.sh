dd6d8b6e4bf11c8c7fe623203aa9135eb0932d20
Matthias Bach <matthias.bach@blue-yonder.com>
2014-12-18 15:17:09 +0100

    Have build preparation script generate Eclipse projects

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,3 +1,3 @@
 mkdir -p ../cpp_odbc_build
 cd ../cpp_odbc_build 
-cmake -DCMAKE_MODULE_PATH=/home/localdata/${USER}/workspace/cmake_scripts ../cpp_odbc
+cmake -DCMAKE_MODULE_PATH=/home/localdata/${USER}/workspace/cmake_scripts -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../cpp_odbc
