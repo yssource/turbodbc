af790a6e5e39c25043d68316e1c1b3e095fc907f
Michael König <michael.koenig@blue-yonder.com>
2014-12-18 19:04:41 +0100

    Added boost and gcc to setup_env script

--- setup_env.sh
+++ setup_env.sh
@@ -2,6 +2,24 @@ BY_HOME_BASE=/home/localdata/${USER}
 
 export CPP_ODBC_LIB_DIR=${BY_HOME_BASE}/workspace/cpp_odbc_build//Library
 export CPP_ODBC_INCLUDE_DIR=${BY_HOME_BASE}/workspace/cpp_odbc/Library/
+
+if [ -z "$label" ]; then 
+        if grep -q Ubuntu /etc/issue; then
+                label=Ubuntu_12.04
+        elif grep -q Enterprise /etc/issue; then
+                label=SUSE_LINUX_11
+        elif grep -q -i debian /etc/issue; then
+                label=Debian_7
+        else 
+                echo "Couldn't determine distribution and 'label' is not set"
+                exit 1
+        fi
+fi
+
+CENTRAL_SOFTWARE_REPOSITORY=/data/software/${label}
+source ${CENTRAL_SOFTWARE_REPOSITORY}/gcc/gcc4.7.2/use_gcc.sh
+source ${CENTRAL_SOFTWARE_REPOSITORY}/boost/1.55_gcc4.7.2_python2.7.3/use_boost.sh
+
 source /data/software/foundation/release/cppunit_toolbox/${label}/cppunit_toolbox_0_3_0/use_cppunit_toolbox.sh
 source /data/software/${label}/unixODBC/unixODBC-2.2.14/use_unixodbc.sh
 
