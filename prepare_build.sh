fd89aa37c9381f04213f1c21a91ecbe708fda2c5
Matthias Bach <matthias.bach@blue-yonder.com>
2014-12-18 15:14:18 +0100

    Make path to additional CMake modules a little less user specific

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,3 +1,3 @@
 mkdir -p ../cpp_odbc_build
 cd ../cpp_odbc_build 
-cmake -DCMAKE_MODULE_PATH=/localdata/mkoenig/workspace/cmake_scripts ../cpp_odbc
+cmake -DCMAKE_MODULE_PATH=/home/localdata/${USER}/workspace/cmake_scripts ../cpp_odbc
