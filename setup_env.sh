cb20739cfe83b325325bada1e16afc40233e193b
Matthias Bach <matthias.bach@blue-yonder.com>
2014-12-18 16:19:24 +0100

    Make environment setup a little less user specific

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,7 @@
 source /data/software/${label}/psapp/latest/use_psapp.sh
 export PSAPP_HOME=$PSAPP_HOME
-export CPP_ODBC_LIB_DIR=/localdata/mkoenig/workspace/cpp_odbc_build//Library
-export CPP_ODBC_INCLUDE_DIR=/localdata/mkoenig/workspace/cpp_odbc/Library/
+export CPP_ODBC_LIB_DIR=/home/localdata/${USER}/workspace/cpp_odbc_build//Library
+export CPP_ODBC_INCLUDE_DIR=/home/localdata/${USER}/workspace/cpp_odbc/Library/
 source /data/software/foundation/release/cppunit_toolbox/${label}/cppunit_toolbox_0_3_0/use_cppunit_toolbox.sh
 source /data/software/${label}/unixODBC/unixODBC-2.2.14/use_unixodbc.sh
 
