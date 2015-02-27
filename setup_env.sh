0d585f008a5d042e4dcf206c27271583b831cbf2
Michael König <michael.koenig@blue-yonder.com>
2015-02-27 16:43:30 +0100

    Enhanced environment script to enable coverage on Jenkins

--- setup_env.sh
+++ setup_env.sh
@@ -17,9 +17,15 @@ if [ -z "$label" ]; then
 fi
 
 CENTRAL_SOFTWARE_REPOSITORY=/data/software/${label}
-source ${CENTRAL_SOFTWARE_REPOSITORY}/gcc/gcc4.7.2/use_gcc.sh
-source ${CENTRAL_SOFTWARE_REPOSITORY}/boost/1.55_gcc4.7.2_python2.7.3/use_boost.sh
-source ${CENTRAL_SOFTWARE_REPOSITORY}/python/2.7.3/use_python.sh
+
+if [ ! -n "$ENVIRONMENT_ROOT" ]
+then
+	ENVIRONMENT_ROOT=${CENTRAL_SOFTWARE_REPOSITORY}
+fi
+
+source $ENVIRONMENT_ROOT/gcc/gcc4.7.2/use_gcc.sh
+source $ENVIRONMENT_ROOT/boost/1.55_gcc4.7.2_python2.7.3/use_boost.sh
+source $ENVIRONMENT_ROOT/python/2.7.3/use_python.sh
 
 source /data/software/foundation/release/cppunit_toolbox/${label}/cppunit_toolbox_0_3_0/use_cppunit_toolbox.sh
 source /data/software/${label}/unixODBC/unixODBC-2.2.14/use_unixodbc.sh
