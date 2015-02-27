d6901dff3ccaef3c31e939c99ab970be6bb97d2e
Michael König <michael.koenig@blue-yonder.com>
2015-02-27 11:39:44 +0100

    Added more test to check retrieval buffer is properly managed

--- Test/tests/result_set_test.cpp
+++ Test/tests/result_set_test.cpp
@@ -27,10 +27,15 @@ CPPUNIT_TEST_SUITE( result_set_test );
 
 	CPPUNIT_TEST( enables_fetching_multiple_rows );
 	CPPUNIT_TEST( fetch_without_columns );
+	CPPUNIT_TEST( fetch_empty_column );
 	CPPUNIT_TEST( fetch_with_single_string_column );
 	CPPUNIT_TEST( fetch_with_single_integer_column );
 	CPPUNIT_TEST( fetch_with_multiple_columns );
-	CPPUNIT_TEST( fetch_with_multiple_rows );
+	CPPUNIT_TEST( fetch_rows_2_buffer_3 );
+	CPPUNIT_TEST( fetch_rows_3_buffer_3 );
+	CPPUNIT_TEST( fetch_rows_4_buffer_3 );
+	CPPUNIT_TEST( fetch_rows_6_buffer_3 );
+
 
 CPPUNIT_TEST_SUITE_END();
 
@@ -38,10 +43,15 @@ public:
 
 	void enables_fetching_multiple_rows();
 	void fetch_without_columns();
+	void fetch_empty_column();
 	void fetch_with_single_string_column();
 	void fetch_with_single_integer_column();
 	void fetch_with_multiple_columns();
 	void fetch_with_multiple_rows();
+	void fetch_rows_2_buffer_3();
+	void fetch_rows_3_buffer_3();
+	void fetch_rows_4_buffer_3();
+	void fetch_rows_6_buffer_3();
 
 };
 
@@ -130,6 +140,35 @@ void result_set_test::fetch_without_columns()
 
 
 namespace {
+	// store a value in a given pointer location
+	ACTION_P2(set_rows_fetched, pointer_to_buffer, value) {
+		*pointer_to_buffer = value;
+	}
+}
+
+void result_set_test::fetch_empty_column()
+{
+	auto statement = prepare_mock_with_columns({SQL_VARCHAR});
+	auto buffers = expect_calls_to_bind_buffer(*statement, {SQL_CHAR});
+
+	SQLULEN * rows_fetched = nullptr;
+	expect_rows_fetched_pointer_set(*statement, rows_fetched);
+
+	auto result_set = pydbc::result_set(statement, 1);
+	CPPUNIT_ASSERT(buffers[0] != nullptr);
+
+	EXPECT_CALL(*statement, do_fetch_next())
+		.WillOnce(testing::DoAll(
+					set_rows_fetched(rows_fetched, 0),
+					testing::Return(false)
+				));
+
+	auto row = result_set.fetch_one();
+	CPPUNIT_ASSERT_EQUAL(0, row.size());
+}
+
+
+namespace {
 	/**
 	* @brief Store the given string value as the first value in
 	*        the buffer pointed to by pointer_to_buffer
@@ -139,12 +178,7 @@ namespace {
 		std::memcpy(element.data_pointer, value.data(), value.size() + 1);
 		element.indicator = value.size();
 	}
-
-	ACTION_P2(set_rows_fetched, pointer_to_buffer, value) {
-		*pointer_to_buffer = value;
-	}
 }
-
 
 void result_set_test::fetch_with_single_string_column()
 {
@@ -247,24 +281,92 @@ void result_set_test::fetch_with_multiple_columns()
 }
 
 
-void result_set_test::fetch_with_multiple_rows()
-{
-	auto statement = prepare_mock_with_columns({SQL_INTEGER});
-	auto buffers = expect_calls_to_bind_buffer(*statement, {SQL_C_SBIGINT});
+namespace {
 
-	SQLULEN * rows_fetched = nullptr;
-	expect_rows_fetched_pointer_set(*statement, rows_fetched);
+	void test_fetch_multiple_rows(std::size_t rows, std::size_t buffer_size)
+	{
+		auto statement = prepare_mock_with_columns({SQL_INTEGER});
+		auto buffers = expect_calls_to_bind_buffer(*statement, {SQL_C_SBIGINT});
 
-	auto result_set = pydbc::result_set(statement, 1);
+		SQLULEN * rows_fetched = nullptr;
+		expect_rows_fetched_pointer_set(*statement, rows_fetched);
 
-	std::vector<long> const expected_values = {42, 17};
-	EXPECT_CALL(*statement, do_fetch_next())
-		.WillOnce(testing::DoAll(
-					put_binary_values_in_buffer(buffers[0], expected_values),
-					set_rows_fetched(rows_fetched, 2),
-					testing::Return(true)
-				));
+		auto result_set = pydbc::result_set(statement, buffer_size);
 
-	CPPUNIT_ASSERT_EQUAL(expected_values[0], boost::get<long>(*result_set.fetch_one()[0]));
-	CPPUNIT_ASSERT_EQUAL(expected_values[1], boost::get<long>(*result_set.fetch_one()[0]));
+		std::vector<long> expected_values;
+		for (std::size_t i = 0; i != rows; ++i) {
+			expected_values.push_back(42 + i);
+		}
+
+		auto const complete_fetches = (rows / buffer_size);
+		auto const remainder = rows % buffer_size;
+
+		// prepare database api calls: first batches to fill the buffer, then
+		// the last batch, then an empty batch
+		testing::InSequence sequence;
+		for (std::size_t i = 0; i != complete_fetches; ++i) {
+			std::vector<long> fetched_data;
+			for (std::size_t j = 0; j != buffer_size; ++j) {
+				fetched_data.push_back(expected_values[i * buffer_size + j]);
+			}
+
+			EXPECT_CALL(*statement, do_fetch_next())
+				.WillOnce(testing::DoAll(
+							put_binary_values_in_buffer(buffers[0], fetched_data),
+							set_rows_fetched(rows_fetched, fetched_data.size()),
+							testing::Return(true)
+						))
+				.RetiresOnSaturation();
+		}
+
+		if (remainder > 0) {
+			std::vector<long> fetched_data;
+			for (std::size_t j = 0; j != remainder; ++j) {
+				fetched_data.push_back(expected_values[complete_fetches * buffer_size + j]);
+			}
+
+			EXPECT_CALL(*statement, do_fetch_next())
+				.WillOnce(testing::DoAll(
+							put_binary_values_in_buffer(buffers[0], fetched_data),
+							set_rows_fetched(rows_fetched, fetched_data.size()),
+							testing::Return(true)
+						))
+				.RetiresOnSaturation();
+		}
+
+		EXPECT_CALL(*statement, do_fetch_next())
+			.WillOnce(testing::DoAll(
+						set_rows_fetched(rows_fetched, 0),
+						testing::Return(false)
+					));
+
+		// assertions
+		for (auto const & value : expected_values) {
+			CPPUNIT_ASSERT_EQUAL( value, boost::get<long>(*result_set.fetch_one()[0]) );
+		}
+		CPPUNIT_ASSERT( result_set.fetch_one().empty() );
+	}
+
+
 }
+
+void result_set_test::fetch_rows_2_buffer_3()
+{
+	test_fetch_multiple_rows(2, 3);
+}
+
+void result_set_test::fetch_rows_3_buffer_3()
+{
+	test_fetch_multiple_rows(3, 3);
+}
+
+void result_set_test::fetch_rows_4_buffer_3()
+{
+	test_fetch_multiple_rows(4, 3);
+}
+
+void result_set_test::fetch_rows_6_buffer_3()
+{
+	test_fetch_multiple_rows(6, 3);
+}
+
