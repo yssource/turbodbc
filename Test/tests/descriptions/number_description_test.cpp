08691b8cc9422e5678d183414bb093062df0882b
Michael König <michael.koenig@blue-yonder.com>
2015-03-20 15:40:47 +0100

    Add tests for number description.

--- Test/tests/descriptions/number_description_test.cpp
+++ Test/tests/descriptions/number_description_test.cpp
@@ -3,6 +3,8 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit_toolbox/extensions/assert_equal_with_different_types.h>
 #include <sqlext.h>
+#include <limits.h>
+#include <stdexcept>
 
 
 class number_description_test : public CppUnit::TestFixture {
@@ -10,6 +12,9 @@ CPPUNIT_TEST_SUITE( number_description_test );
 
 	CPPUNIT_TEST( positive_integer );
 	CPPUNIT_TEST( negative_integer );
+	CPPUNIT_TEST( max_positive_integer );
+	CPPUNIT_TEST( min_negative_integer );
+	CPPUNIT_TEST( larger_than_max_positive_overflows );
 
 CPPUNIT_TEST_SUITE_END();
 
@@ -17,6 +22,9 @@ public:
 
 	void positive_integer();
 	void negative_integer();
+	void max_positive_integer();
+	void min_negative_integer();
+	void larger_than_max_positive_overflows();
 
 };
 
@@ -55,4 +63,53 @@ void number_description_test::negative_integer()
 	auto const actual = description.make_field(reinterpret_cast<char const *>(&data));
 
 	CPPUNIT_ASSERT_EQUAL(pydbc::field{expected}, actual);
+}
+
+void number_description_test::max_positive_integer()
+{
+	SQL_NUMERIC_STRUCT data = {
+				18,
+				0,
+				1, // + sign
+				// binary representation of 2^63 - 1
+				{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+			};
+
+	long const expected = std::numeric_limits<long>::max();
+	pydbc::number_description description;
+	auto const actual = description.make_field(reinterpret_cast<char const *>(&data));
+
+	CPPUNIT_ASSERT_EQUAL(pydbc::field{expected}, actual);
+}
+
+void number_description_test::min_negative_integer()
+{
+	SQL_NUMERIC_STRUCT data = {
+				18,
+				0,
+				0, // + sign
+				// binary representation of 2^63
+				{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+			};
+
+	long const expected = std::numeric_limits<long>::min();
+	pydbc::number_description description;
+	auto const actual = description.make_field(reinterpret_cast<char const *>(&data));
+
+	CPPUNIT_ASSERT_EQUAL(pydbc::field{expected}, actual);
+}
+
+void number_description_test::larger_than_max_positive_overflows()
+{
+	SQL_NUMERIC_STRUCT data = {
+				18,
+				0,
+				1, // + sign
+				// binary representation of 2^63 = maximum positive integer + 1
+				{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+			};
+
+	pydbc::number_description description;
+	std::cout << "\n\n" << description.make_field(reinterpret_cast<char const *>(&data)) << "\n\n";
+	CPPUNIT_ASSERT_THROW(description.make_field(reinterpret_cast<char const *>(&data)), std::overflow_error);
 }
