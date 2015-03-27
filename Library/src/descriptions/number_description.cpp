da9f891a112d857cdc35d2ca75a7fda06d1d6ae9
Michael König <michael.koenig@blue-yonder.com>
2015-03-27 10:25:41 +0100

    Extract low-level stuff in number_description

--- Library/src/descriptions/number_description.cpp
+++ Library/src/descriptions/number_description.cpp
@@ -10,6 +10,38 @@ namespace pydbc {
 number_description::number_description() = default;
 number_description::~number_description() = default;
 
+namespace {
+
+	boost::multiprecision::checked_cpp_int extract_signed_mantissa(SQL_NUMERIC_STRUCT const & value)
+	{
+		boost::multiprecision::checked_cpp_int mantissa = 0;
+		for (unsigned int i = SQL_MAX_NUMERIC_LEN; i != 0; --i) {
+			unsigned char const byte = value.val[i - 1];
+			mantissa <<= 8;
+			mantissa += byte;
+		}
+
+		if (value.sign != 1) {
+			mantissa *= -1;
+		}
+
+		return mantissa;
+	}
+
+	using floating = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<36>>;
+
+	floating extract_exponent(SQL_NUMERIC_STRUCT const & value)
+	{
+		return boost::multiprecision::pow(floating(0.1), static_cast<int>(value.scale));
+	}
+
+	bool is_integer(SQL_NUMERIC_STRUCT const & value)
+	{
+		return value.scale == 0;
+	}
+
+}
+
 std::size_t number_description::do_element_size() const
 {
 	return sizeof(SQL_NUMERIC_STRUCT);
@@ -20,27 +52,17 @@ SQLSMALLINT number_description::do_column_type() const
 	return SQL_NUMERIC;
 }
 
-// TODO extract bits and pieces! very low-level!
+
 field number_description::do_make_field(char const * data_pointer) const
 {
 	auto numeric_ptr = reinterpret_cast<SQL_NUMERIC_STRUCT const *>(data_pointer);
 
-	boost::multiprecision::checked_cpp_int mantissa = 0;
-	for (unsigned int i = SQL_MAX_NUMERIC_LEN; i != 0; --i) {
-		unsigned char const byte = numeric_ptr->val[i - 1];
-		mantissa <<= 8;
-		mantissa += byte;
-	}
+	auto const mantissa = extract_signed_mantissa(*numeric_ptr);
 
-	if (numeric_ptr->sign != 1) {
-		mantissa *= -1;
-	}
-
-	if (numeric_ptr->scale == 0) {
+	if (is_integer(*numeric_ptr)) {
 		return {mantissa.convert_to<long>()};
 	} else {
-		using floating = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<36>>;
-		floating exponent = boost::multiprecision::pow(floating(0.1), static_cast<int>(numeric_ptr->scale));
+		auto exponent = extract_exponent(*numeric_ptr);
 		exponent *= static_cast<floating>(mantissa);
 		return {exponent.convert_to<double>()};
 	}
--- prepare_build.sh
+++ prepare_build.sh
@@ -1,3 +1,4 @@
+BY_HOME_BASE=/localdata/${USER}
 mkdir -p ../pydbc_build
 cd ../pydbc_build 
-cmake -DCMAKE_MODULE_PATH="${BY_HOME_BASE}/workspace/cmake_scripts" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../pydbc
+cmake -DCMAKE_MODULE_PATH="${BY_HOME_BASE}/workspace/techteam_build/cmake_scripts" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../pydbc
