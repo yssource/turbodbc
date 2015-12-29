b08b900db4d1a5e3ce85af737cc280bfd727d6d1
Michael König <michael.koenig@blue-yonder.com>
2015-12-29 17:11:14 +0100

    Remove code duplication in select suite

--- PyTest/test_select.py
+++ PyTest/test_select.py
@@ -3,6 +3,12 @@ import pydbc
 
 
 class SelectBaseTestCase(object):
+    """
+    Children are expected to provide the following attributes:
+    
+    self.dsn
+    self.supports_row_count
+    """
 
     def setUp(self):
         self.connection = pydbc.connect(self.dsn)
@@ -12,30 +18,35 @@ class SelectBaseTestCase(object):
         self.cursor.close()
         self.connection.close()
 
-    def test_single_row_NULL_result(self):
-        self.cursor.execute("select NULL")
-        self.assertIn(self.cursor.rowcount, [-1, 1])
+    def _test_single_row_result_set(self, query, expected_row):
+        self.cursor.execute(query)
+
+        if self.supports_row_count:
+            self.assertEqual(self.cursor.rowcount, 1)
+        else:
+            self.assertEqual(self.cursor.rowcount, -1)
+
         row = self.cursor.fetchone()
-        self.assertItemsEqual(row, [None])
+        self.assertItemsEqual(row, expected_row)
+
         row = self.cursor.fetchone()
         self.assertIsNone(row)
 
+    def test_single_row_NULL_result(self):
+        self._test_single_row_result_set("SELECT NULL", [None])
+
     def test_single_row_integer_result(self):
-        self.cursor.execute("select 42")
-        self.assertIn(self.cursor.rowcount, [-1, 1])
-        row = self.cursor.fetchone()
-        self.assertItemsEqual(row, [42])
-        row = self.cursor.fetchone()
-        self.assertIsNone(row)
- 
+        self._test_single_row_result_set("SELECT 42", [42])
+
     def test_single_row_string_result(self):
-        self.cursor.execute("select 'Oh Boy!'")
-        self.assertIn(self.cursor.rowcount, [-1, 1])
-        row = self.cursor.fetchone()
-        self.assertItemsEqual(row, ['Oh Boy!'])
-        row = self.cursor.fetchone()
-        self.assertIsNone(row)
- 
+        self._test_single_row_result_set("SELECT 'value'", ["value"])
+
+    def test_single_row_large_numeric_result_as_string(self):
+        self._test_single_row_result_set("SELECT -1234567890123.123456789", ['-1234567890123.123456789'])
+
+    def test_single_row_multiple_integer_result(self):
+        self._test_single_row_result_set("SELECT 40, 41, 42, 43", [40, 41, 42, 43])
+
     def test_single_row_double_result(self):
         self.cursor.execute("select a from test_read_double")
         self.assertIn(self.cursor.rowcount, [-1, 1])
@@ -43,23 +54,7 @@ class SelectBaseTestCase(object):
         self.assertItemsEqual(row, [3.14])
         row = self.cursor.fetchone()
         self.assertIsNone(row)
- 
-    def test_single_row_large_numeric_result(self):
-        self.cursor.execute("select -1234567890123.123456789")
-        self.assertIn(self.cursor.rowcount, [-1, 1])
-        row = self.cursor.fetchone()
-        self.assertItemsEqual(row, ['-1234567890123.123456789'])
-        row = self.cursor.fetchone()
-        self.assertIsNone(row)
- 
-    def test_single_row_multiple_integer_result(self):
-        self.cursor.execute("select 40, 41, 42, 43")
-        self.assertIn(self.cursor.rowcount, [-1, 1])
-        row = self.cursor.fetchone()
-        self.assertItemsEqual(row, [40, 41, 42, 43])
-        row = self.cursor.fetchone()
-        self.assertIsNone(row)
- 
+
     def test_multiple_row_iterate_result(self):
         self.cursor.execute("delete from test_integer")
         for i in xrange(1,10):
@@ -69,11 +64,18 @@ class SelectBaseTestCase(object):
             self.assertItemsEqual([element[0]], element[1])
 
 
+# Actual test cases
+
 class TestSelectExasol(SelectBaseTestCase, TestCase):
     dsn = "Exasol R&D test database"
+    supports_row_count = True
+
 
 class TestSelectPostgreSQL(SelectBaseTestCase, TestCase):
     dsn = "PostgreSQL R&D test database"
+    supports_row_count = False
+
 
 class TestSelectMySQL(SelectBaseTestCase, TestCase):
     dsn = "MySQL R&D test database"
+    supports_row_count = True
