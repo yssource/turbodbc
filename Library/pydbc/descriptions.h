46be0a2ade8d2fccd2d4bad4fc1eae02fb8db14f
Michael König <michael.koenig@blue-yonder.com>
2015-12-29 14:34:35 +0100

    Remove number_description. Does not work with all drivers since some flags are ignored/not set

--- Library/pydbc/descriptions.h
+++ Library/pydbc/descriptions.h
@@ -3,5 +3,4 @@
 #include <pydbc/descriptions/boolean_description.h>
 #include <pydbc/descriptions/floating_point_description.h>
 #include <pydbc/descriptions/integer_description.h>
-#include <pydbc/descriptions/number_description.h>
 #include <pydbc/descriptions/string_description.h>
