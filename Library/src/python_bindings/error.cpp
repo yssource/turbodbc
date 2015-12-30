50a85cbc9c554bce930442812e03f2ff5edaf5d4
Michael König <michael.koenig@blue-yonder.com>
2015-12-30 10:49:31 +0100

    Reformat code slightly

--- Library/src/python_bindings/error.cpp
+++ Library/src/python_bindings/error.cpp
@@ -24,10 +24,12 @@ PyObject* createExceptionClass(const char* name, PyObject* baseTypeObj = PyExc_S
 
 	string scopeName = bp::extract<string>(bp::scope().attr("__name__"));
 	string qualifiedName0 = scopeName + "." + name;
-	char* qualifiedName1 = const_cast<char*>(qualifiedName0.c_str());
+	char* qualifiedName1 = const_cast<char *>(qualifiedName0.c_str());
 
 	PyObject* typeObj = PyErr_NewException(qualifiedName1, baseTypeObj, 0);
-	if(!typeObj) bp::throw_error_already_set();
+	if (not typeObj) {
+		bp::throw_error_already_set();
+	}
 	bp::scope().attr(name) = bp::handle<>(bp::borrowed(typeObj));
 	return typeObj;
 }
