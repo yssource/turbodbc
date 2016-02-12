d9c1362ce7a69be039da9ea7478c398a2996ef99
Michael König <michael.koenig@blue-yonder.com>
2016-02-12 11:10:55 +0100

    Fix cmake build to compile cpp_odbc as well

--- Findcpp_odbc.cmake
+++
@@ -1,5 +0,0 @@
-# This script provides the currently set cpp_odbc environment variables as
-# include and link directories
-
-include_directories(SYSTEM $ENV{CPP_ODBC_INCLUDE_DIR})
-link_directories($ENV{CPP_ODBC_LIB_DIR})
