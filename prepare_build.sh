06b29485d049e42f1d31bb3a7a6ca483ac67ec47
Michael König <michael.koenig@blue-yonder.com>
2016-03-18 13:36:45 +0100

    Remove Blue Yonder specific files

--- prepare_build.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ../pydbc_build
-cd ../pydbc_build 
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../pydbc
