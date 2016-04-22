650f825c117d3b7638ff3cc0da56aa31b059cc80
Michael König <michael.koenig@blue-yonder.com>
2016-04-22 16:32:24 +0200

    Migrate test_data_types to pytest

--- python/turbodbc_test/test_data_types.py
+++ python/turbodbc_test/test_data_types.py
@@ -1,5 +1,3 @@
-from unittest import TestCase
-
 import turbodbc.data_types
 from turbodbc import STRING, BINARY, NUMBER, DATETIME, ROWID
 
@@ -12,14 +10,15 @@ ALL_TYPE_CODES = [turbodbc.data_types._BOOLEAN_CODE,
 
 ALL_DATA_TYPES = [STRING, BINARY, NUMBER, DATETIME, ROWID]
 
-class TestDataTypes(TestCase):
-    def test_each_type_code_matches_one_data_type(self):
-        for type_code in ALL_TYPE_CODES:
-            matches = [type for type in ALL_DATA_TYPES if type_code == type]
-            self.assertEqual(1, len(matches))
 
-    def test_each_type_code_matches_all_but_one_data_type(self):
-        for type_code in ALL_TYPE_CODES:
-            mismatches = [type for type in ALL_DATA_TYPES if type_code != type]
-            expected = len(ALL_DATA_TYPES) - 1
-            self.assertEqual(expected, len(mismatches))
+def test_each_type_code_matches_one_data_type():
+    for type_code in ALL_TYPE_CODES:
+        matches = [type for type in ALL_DATA_TYPES if type_code == type]
+        assert 1 == len(matches)
+
+
+def test_each_type_code_matches_all_but_one_data_type():
+    for type_code in ALL_TYPE_CODES:
+        mismatches = [type for type in ALL_DATA_TYPES if type_code != type]
+        expected = len(ALL_DATA_TYPES) - 1
+        assert expected == len(mismatches)
