ccc7b60a62a1fe3482b67f9bc4acb444fd9e31a4
Michael König <michael.koenig@blue-yonder.com>
2016-04-22 16:22:28 +0200

    Migrate test_constructors to pytest

--- python/turbodbc_test/test_constructors.py
+++ python/turbodbc_test/test_constructors.py
@@ -1,18 +1,15 @@
-from unittest import TestCase
 import datetime
 
 from turbodbc import Date, Time, Timestamp
 
+def test_constructors_date():
+    d = Date(2016, 1, 4)
+    assert d == datetime.date(2016, 1, 4)
 
-class TestConstructors(TestCase):
-    def test_date(self):
-        d = Date(2016, 1, 4)
-        self.assertEqual(d, datetime.date(2016, 1, 4))
+def test_constructors_time():
+    t = Time(1, 2, 3)
+    assert t == datetime.time(1, 2, 3)
 
-    def test_time(self):
-        t = Time(1, 2, 3)
-        self.assertEqual(t, datetime.time(1, 2, 3))
-
-    def test_timestamp(self):
-        ts = Timestamp(2016, 1, 2, 3, 4, 5)
-        self.assertEqual(ts, datetime.datetime(2016, 1, 2, 3, 4, 5))
+def test_constructors_timestamp():
+    ts = Timestamp(2016, 1, 2, 3, 4, 5)
+    assert ts == datetime.datetime(2016, 1, 2, 3, 4, 5)
