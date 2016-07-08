fe65cc33b6e3d8b52a87be5703f114dcb381947d
Michael König <michael.koenig@blue-yonder.com>
2016-07-08 17:28:27 +0200

    Split turbodbc_intern into libraries with and without Python bindings. Adjust setup.py to build three extensions (main lib, Python bindings, Numpy bindings)

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 import sys
+import sysconfig
 import os
 import os.path as op
 import distutils.spawn as ds
@@ -11,10 +12,25 @@ import distutils
 import itertools
 from glob import iglob
 
-def _get_source_files():
-    iterable_sources = (iglob(os.path.join(root,'*.cpp')) for root, dirs, files in os.walk('src'))
+
+def _get_distutils_build_directory():
+    """
+    Returns the directory distutils uses to build its files.
+    We need this directory since we build extensions which have to link
+    other ones.
+    """
+    pattern = "lib.{platform}-{major}.{minor}"
+    return os.path.join('build', pattern.format(platform=sysconfig.get_platform(),
+                                                major=sys.version_info[0],
+                                                minor=sys.version_info[1]))
+
+
+def _get_source_files(directory):
+    path = os.path.join('src', directory)
+    iterable_sources = (iglob(os.path.join(root,'*.cpp')) for root, dirs, files in os.walk(path))
     source_files = itertools.chain.from_iterable(iterable_sources)
     return list(source_files)
+
 
 def _remove_strict_prototype_option_from_distutils_config():
     strict_prototypes = '-Wstrict-prototypes'
@@ -25,15 +41,42 @@ def _remove_strict_prototype_option_from_distutils_config():
 
 _remove_strict_prototype_option_from_distutils_config()
 
+turbodbc_sources = _get_source_files('cpp_odbc') + _get_source_files('turbodbc')
 
-turbodbc_intern = distutils.core.Extension('turbodbc_intern',
-                                           sources=_get_source_files(),
+"""
+Extension module which is actually a plain C++ library without Python bindings
+"""
+turbodbc_library = distutils.core.Extension('libturbodbc',
+                                            sources=turbodbc_sources,
+                                            include_dirs=['include/'],
+                                            extra_compile_args=['--std=c++11'],
+                                            libraries=['odbc', 'boost_python'])
+
+"""
+An extension module which contains the main Python bindings for turbodbc
+"""
+turbodbc_python = distutils.core.Extension('turbodbc_intern',
+                                           sources=_get_source_files('turbodbc_python'),
                                            include_dirs=['include/'],
                                            extra_compile_args=['--std=c++11'],
-                                           libraries=['odbc', 'boost_python'])
+                                           libraries=['odbc', 'boost_python', 'turbodbc'],
+                                           extra_link_args=["-Wl,-rpath,$ORIGIN"],
+                                           library_dirs=[_get_distutils_build_directory()])
+
+"""
+An extension module which contains Python bindings which require numpy support
+to work. Not included in the standard Python bindings so this can stay optional.
+"""
+turbodbc_numpy = distutils.core.Extension('turbodbc_numpy',
+                                           sources=_get_source_files('turbodbc_numpy'),
+                                           include_dirs=['include/'],
+                                           extra_compile_args=['--std=c++11'],
+                                           libraries=['odbc', 'boost_python', 'turbodbc'],
+                                           extra_link_args=["-Wl,-rpath,$ORIGIN"],
+                                           library_dirs=[_get_distutils_build_directory()])
 
 setup(name = 'turbodbc',
-      version = '0.3.0',
+      version = '0.4.0',
       description = 'turbodbc is a Python DB API 2.0 compatible ODBC driver',
       include_package_data = True,
       url = 'https://github.com/blue-yonder/turbodbc',
@@ -47,5 +90,5 @@ setup(name = 'turbodbc',
                      'Programming Language :: C++',
                      'Programming Language :: Python :: 2.7',
                      'Topic :: Database'],
-      ext_modules = [turbodbc_intern]
+      ext_modules = [turbodbc_library, turbodbc_python, turbodbc_numpy]
       )
