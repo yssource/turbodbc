8ce27c8a96e8de202cd5e72ceb7fc78d0330f966
Michael König <michael.koenig@blue-yonder.com>
2016-07-15 17:01:43 +0200

    Fix tests for databases which return lower-case column names

--- python/turbodbc_test/test_select_numpy.py
+++ python/turbodbc_test/test_select_numpy.py
@@ -9,6 +9,17 @@ from query_fixture import query_fixture
 from helpers import open_cursor, for_each_database, for_one_database
 
 
+def _fix_case(configuration, string):
+    """
+    some databases return column names in upper case
+    """
+    capabilities = configuration['capabilities']
+    if capabilities['reports_column_names_as_upper_case']:
+        return string.upper()
+    else:
+        return string
+
+
 @for_one_database
 def test_numpy_without_result_set_raises(dsn, configuration):
     with open_cursor(configuration) as cursor:
@@ -24,7 +35,7 @@ def test_numpy_empty_column(dsn, configuration):
             results = cursor.fetchallnumpy()
             assert isinstance(results, OrderedDict)
             assert len(results) == 1
-            assert isinstance(results['A'], MaskedArray)
+            assert isinstance(results[_fix_case(configuration, 'a')], MaskedArray)
 
 
 @for_each_database
@@ -33,7 +44,7 @@ def test_numpy_int_column(dsn, configuration):
         cursor.execute("SELECT 42 AS a")
         results = cursor.fetchallnumpy()
         expected = MaskedArray([42], mask=[0])
-        assert all(results['A'] == expected)
+        assert all(results[_fix_case(configuration, 'a')] == expected)
 
 
 @for_each_database
@@ -45,7 +56,7 @@ def test_numpy_large_column(dsn, configuration):
             cursor.execute("SELECT a FROM {} ORDER BY a".format(table_name))
             results = cursor.fetchallnumpy()
             expected = MaskedArray([1, 2, 3, 4, 5], mask=False)
-            assert all(results['A'] == expected)
+            assert all(results[_fix_case(configuration, 'a')] == expected)
 
 
 @for_each_database
@@ -56,5 +67,5 @@ def test_numpy_two_columns(dsn, configuration):
                                [[1, 42], [2, 41]])
             cursor.execute("SELECT a, b FROM {} ORDER BY a".format(table_name))
             results = cursor.fetchallnumpy()
-            assert all(results['A'] == MaskedArray([1, 2], mask=False))
-            assert all(results['B'] == MaskedArray([42, 41], mask=False))
+            assert all(results[_fix_case(configuration, 'a')] == MaskedArray([1, 2], mask=False))
+            assert all(results[_fix_case(configuration, 'b')] == MaskedArray([42, 41], mask=False))
