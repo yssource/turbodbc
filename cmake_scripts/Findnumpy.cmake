4f127377a32f0d06b61ab6ccba2834203292836d
Michael König <michael.koenig@blue-yonder.com>
2016-07-15 12:46:45 +0200

    Add cmake script for numpy

---
+++ cmake_scripts/Findnumpy.cmake
@@ -0,0 +1,12 @@
+# This script provides include directories for numpy
+
+find_path(
+    Numpy_INCLUDE_DIR
+    numpy/npy_common.h
+    HINTS
+        $ENV{VIRTUAL_ENV}/lib/*/site-packages/numpy/core/include
+        ENV PYTHON_INCLUDE_DIR
+        /usr/local/lib/python2.7/dist-packages/numpy/core/include
+    DOC "Path to the numpy headers"
+)
+
