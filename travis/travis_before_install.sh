c64dc17b8967b31026dcefa9c62b3ef81f99acc9
Michael König <michael.koenig@blue-yonder.com>
2016-12-30 16:50:41 +0100

    Use pybind11 in setup, build the source distribution still shows build errors, but works nevertheless

--- travis/travis_before_install.sh
+++ travis/travis_before_install.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y python-pytest \
         libboost-all-dev \
         mysql-server-5.6 mysql-client-core-5.6 mysql-client-5.6 libmyodbc \
         postgresql odbc-postgresql=1:09.02.0100-2ubuntu1
-sudo pip install mock numpy==1.8.0
+sudo pip install mock numpy==1.8.0 pybind11==1.8.1
