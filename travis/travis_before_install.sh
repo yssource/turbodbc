ffffe54c9034ae5a9358a0610ec1e7d737481093
Michael König <michael.koenig@blue-yonder.com>
2017-01-10 12:32:27 +0100

    Use more specific boost package in travis installation

--- travis/travis_before_install.sh
+++ travis/travis_before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 sudo apt-get install -y python-pytest \
         unixodbc unixodbc-dev \
-        libboost-all-dev \
+        libboost-date-time-dev \
         mysql-server-5.6 mysql-client-core-5.6 mysql-client-5.6 libmyodbc \
         postgresql odbc-postgresql=1:09.02.0100-2ubuntu1
 sudo pip install mock numpy==1.8.0 pybind11==1.8.1
