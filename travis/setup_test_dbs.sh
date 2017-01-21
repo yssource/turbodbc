85af09717e3948168c31018e5356bf7217514900
Michael König <michael.koenig@blue-yonder.com>
2017-01-21 22:02:05 +0100

    Fix many build errors

--- travis/setup_test_dbs.sh
+++ travis/setup_test_dbs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
-sudo -u postgres psql -U postgres -c 'CREATE DATABASE test_db;'
-sudo -u postgres psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'password';"
-mysql -u root -e 'CREATE DATABASE test_db;'
+
+postgres psql -U postgres -c 'CREATE DATABASE test_db;'
+postgres psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'password';"
+mysql -e 'CREATE DATABASE test_db;'
