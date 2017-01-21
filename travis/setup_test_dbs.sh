f55b1633b740005a2db82d3d80c7b7fd58d61306
Michael König <michael.koenig@blue-yonder.com>
2017-01-21 22:04:51 +0100

    Fix postgres setup

--- travis/setup_test_dbs.sh
+++ travis/setup_test_dbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-postgres psql -U postgres -c 'CREATE DATABASE test_db;'
-postgres psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'password';"
+psql -U postgres -c 'CREATE DATABASE test_db;'
+psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'password';"
 mysql -e 'CREATE DATABASE test_db;'
