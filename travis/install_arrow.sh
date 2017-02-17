ad13b118093863d6bd41f6d0b54e42071998d01c
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-02-17 16:55:49 +0100

    Install pip dev requirements

--- travis/install_arrow.sh
+++ travis/install_arrow.sh
@@ -14,6 +14,7 @@ make install
 popd
 
 pushd python
+pip install numpy cython pandas six
 python setup.py build_ext --inplace
 python setup.py install --single-version-externally-managed --record record.txt
 popd
