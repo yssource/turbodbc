dd8cc129b23ab3bb30a6672198549ccc38b1155f
Michael König <michael.koenig@blue-yonder.com>
2017-03-11 15:40:22 +0100

    Add missing numpy unicode column files

---
+++ cpp/turbodbc_numpy/Library/src/unicode_column.cpp
@@ -0,0 +1,42 @@
+#include <turbodbc_numpy/unicode_column.h>
+
+#include <sql.h>
+#include <cstring>
+
+namespace turbodbc_numpy {
+
+using pybind11::object;
+using pybind11::reinterpret_steal;
+
+unicode_column::unicode_column()
+{
+}
+
+
+unicode_column::~unicode_column() = default;
+
+void unicode_column::do_append(cpp_odbc::multi_value_buffer const & buffer, std::size_t n_values)
+{
+	for (std::size_t i = 0; i != n_values; ++i) {
+		auto const element = buffer[i];
+		if (element.indicator == SQL_NULL_DATA) {
+			data_.append(pybind11::none());
+		} else {
+			data_.append(reinterpret_steal<object>(PyUnicode_DecodeUTF16(element.data_pointer, element.indicator, NULL, NULL)));
+		}
+	}
+}
+
+object unicode_column::do_get_data()
+{
+	return data_;
+}
+
+object unicode_column::do_get_mask()
+{
+	return pybind11::cast(false);
+}
+
+
+}
+
