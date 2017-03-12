adfb437d1d4863849b88f00c1c0c583f133052d6
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-03-12 12:37:24 +0100

    Remove const statements to support Xcode6.4

--- cpp/cpp_odbc/Test/tests/level3/raii_environment_test.cpp
+++ cpp/cpp_odbc/Test/tests/level3/raii_environment_test.cpp
@@ -110,7 +110,7 @@ TEST(RaiiEnvironmentTest, MakeConnection)
 	std::string const connection_string("dummy connection string");
 	auto api = make_default_api();
 
-	auto environment = std::make_shared<raii_environment const>(api);
+	auto environment = std::make_shared<raii_environment>(api);
 	EXPECT_CALL(*api, do_allocate_connection_handle(default_e_handle))
 		.WillOnce(testing::Return(default_c_handle));
 	EXPECT_CALL(*api, do_establish_connection(testing::_, connection_string))
