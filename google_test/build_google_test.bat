c50986f967c556f6da406f1660721481e759663c
Michael König <michael.koenig@blue-yonder.com>
2017-03-26 21:00:31 +0200

    Remove bat file; seems to work fine (or work, actually) with the sh script

--- google_test/build_google_test.bat
+++
@@ -1,7 +0,0 @@
-echo "Cloning latest google test repository"
-git clone https://github.com/google/googletest.git
-
-mkdir build
-cd build
-cmake ../googletest -DCMAKE_INSTALL_PREFIX=../dist
-cmake --build . --target install
