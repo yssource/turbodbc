c0678651317b5f514f10e48917bbec6188502754
TWAC <>
2017-03-29 11:18:36 +0200

    switch to Release build for Windows

--- google_test/build_google_test.bat
+++ google_test/build_google_test.bat
@@ -3,5 +3,5 @@ git clone https://github.com/google/googletest.git
 
 md build
 cd build
-cmake ../googletest -DCMAKE_INSTALL_PREFIX=../dist -DCMAKE_VERBOSE_MAKEFILE=ON -A x64
-cmake --build . --target install
+cmake ../googletest -DCMAKE_INSTALL_PREFIX=../dist -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -A x64
+cmake --build . --config Release --target install
