1ccb0f31df2e100435731c6c791a98f397403842
Michael König <michael.koenig@blue-yonder.com>
2017-04-02 17:48:28 +0200

    Fix artifact section, bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ def get_extension_modules():
 
 
 setup(name = 'turbodbc',
-      version = '1.1.0',
+      version = '1.1.1',
       description = 'turbodbc is a Python DB API 2.0 compatible ODBC driver',
       include_package_data = True,
       url = 'https://github.com/blue-yonder/turbodbc',
