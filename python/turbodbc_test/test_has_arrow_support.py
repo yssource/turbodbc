245303fe283d8aef00d293271e27ef33534c437a
Uwe L. Korn <uwelk@xhochy.com>
2017-05-26 14:08:27 +0200

    Skip even more tests

--- python/turbodbc_test/test_has_arrow_support.py
+++ python/turbodbc_test/test_has_arrow_support.py
@@ -2,10 +2,18 @@ from mock import patch
 
 from turbodbc.cursor import _has_arrow_support
 
+# Skip all parquet tests if we can't import pyarrow.parquet
+pa = pytest.importorskip('pyarrow')
 
+# Ignore these with pytest ... -m 'not parquet'
+pyarrow = pytest.mark.pyarrow
+
+
+@pyarrow
 def test_has_arrow_support_fails():
     with patch('__builtin__.__import__', side_effect=ImportError):
         assert _has_arrow_support() == False
 
+@pyarrow
 def test_has_arrow_support_succeeds():
     assert _has_arrow_support() == True
