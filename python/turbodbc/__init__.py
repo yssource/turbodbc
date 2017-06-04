17194534c21071195c692ad956ecdf9612a9a36a
Michael König <michael.koenig@blue-yonder.com>
2017-06-04 10:12:20 +0200

    Add version variable. Fixes #96

--- python/turbodbc/__init__.py
+++ python/turbodbc/__init__.py
@@ -8,4 +8,9 @@ from .data_types import STRING, BINARY, NUMBER, DATETIME, ROWID
 from .options import make_options
 from turbodbc_intern import Rows, Megabytes
 
+import pkg_resources
 
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except:
+    __version__ = 'unknown'
