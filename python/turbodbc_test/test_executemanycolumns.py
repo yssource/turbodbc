b68b54c0a9eede08511fe4892ad9500a1b5a3ad5
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-09-20 16:11:51 +0200

    Python 2/3 compability

--- python/turbodbc_test/test_executemanycolumns.py
+++ python/turbodbc_test/test_executemanycolumns.py
@@ -240,7 +240,7 @@ def test_multiple_columns(dsn, configuration, column_backend):
         with query_fixture(cursor, configuration, 'INSERT TWO INTEGER COLUMNS') as table_name:
             columns = [array([17, 23, 42], dtype='int64'), array([3, 2, 1], dtype='int64')]
             if column_backend == 'arrow':
-                columns = map(lambda x: pa.Array.from_pandas(x), columns)
+                columns = list(map(lambda x: pa.Array.from_pandas(x), columns))
                 columns = pa.Table.from_arrays(columns, ['column1', 'column2'])
             cursor.executemanycolumns("INSERT INTO {} VALUES (?, ?)".format(table_name), columns)
 
