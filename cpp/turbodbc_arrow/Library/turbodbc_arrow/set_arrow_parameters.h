1f615eae4115f493ee06baf6832a2c6273a1fd57
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-09-21 17:28:25 +0200

    Always undef

--- cpp/turbodbc_arrow/Library/turbodbc_arrow/set_arrow_parameters.h
+++ cpp/turbodbc_arrow/Library/turbodbc_arrow/set_arrow_parameters.h
@@ -3,9 +3,7 @@
 #include <turbodbc/parameter_sets/bound_parameter_set.h>
 
 #undef BOOL
-#if _MSC_VER >= 1900
-  #undef timezone
-#endif
+#undef timezone
 #include <arrow/api.h>
 #include <arrow/python/pyarrow.h>
 #include <pybind11/pybind11.h>
