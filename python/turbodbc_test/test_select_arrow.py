67f3a41a19c5a3631c75e2c28f53a13d8ae567cd
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-10-17 20:18:30 +0200

    Only test relevant assertions

--- python/turbodbc_test/test_select_arrow.py
+++ python/turbodbc_test/test_select_arrow.py
@@ -89,10 +89,6 @@ def test_arrow_int_column_adaptive(dsn, configuration):
     with open_cursor(configuration) as cursor:
         cursor.execute("SELECT 42 AS a")
         result = cursor.fetchallarrow(adaptive_integers=True)
-        assert isinstance(result, pa.Table)
-        assert result.num_columns == 1
-        assert result.num_rows == 1
-        assert result.column(0).name == _fix_case(configuration, "a")
         assert str(result.column(0).type) == "int8"
         assert result.column(0).to_pylist() == [42]
 
